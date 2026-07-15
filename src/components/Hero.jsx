4c66a6d739912a569764604ed7638ce1848860f8
suryansh <suryanshgoel7017@gmail.com>
2026-07-15 21:49:39 +0530

    Day 3: Build responsive navbar and hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+import "./../styles/Hero.css";
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Fashion That Speaks</h1>
+
+        <p>
+          Discover the latest trends in fashion with exclusive deals and
+          premium brands.
+        </p>
+
+        <button className="shop-btn">Shop Now</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
